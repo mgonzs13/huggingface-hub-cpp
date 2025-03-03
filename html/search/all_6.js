565d405dd50622d28af121c941089e671dac6d25
mgonzs13 <mgonzs13@users.noreply.github.com>
2025-03-03 20:32:32 +0000

    deploy: a2a51214fe1e880f1701d1c56de3b4c2857b055c

--- html/search/all_6.js
+++ html/search/all_6.js
@@ -1,7 +1,8 @@
 var searchData=
 [
   ['hf_5fhub_5fdownload_0',['hf_hub_download',['../huggingface__hub_8h.html#ad3bc4085f79006e58fb551cadf2083aa',1,'huggingface_hub']]],
-  ['hub_20cpp_1',['huggingface-hub-cpp',['../index.html',1,'']]],
-  ['huggingface_20hub_20cpp_2',['huggingface-hub-cpp',['../index.html',1,'']]],
-  ['huggingface_5fhub_2eh_3',['huggingface_hub.h',['../huggingface__hub_8h.html',1,'']]]
+  ['hf_5fhub_5fdownload_5fwith_5fshards_1',['hf_hub_download_with_shards',['../huggingface__hub_8h.html#a00d6e3b2adbdc5459618c8eef9491c04',1,'huggingface_hub']]],
+  ['hub_20cpp_2',['huggingface-hub-cpp',['../index.html',1,'']]],
+  ['huggingface_20hub_20cpp_3',['huggingface-hub-cpp',['../index.html',1,'']]],
+  ['huggingface_5fhub_2eh_4',['huggingface_hub.h',['../huggingface__hub_8h.html',1,'']]]
 ];
